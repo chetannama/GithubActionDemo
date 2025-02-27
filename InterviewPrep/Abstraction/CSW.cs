e3b44cc011a6127f9a4e0d3de4a3bfadb7fb4062
ChetanNama <chetan.nama2794@gmail.com>
2025-02-27 23:45:40 +0530

    First commit

---
+++ InterviewPrep/Abstraction/CSW.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.Abstraction
+{
+    public class CSW : BaseEmployee
+    {
+        public double RatePerHour { get; set; }
+        public double TotalHours { get; set; }
+
+        public override double MonthlySalary()
+        {
+            return RatePerHour * TotalHours;
+        }
+    }
+}
---
+++ InterviewPrep/Abstraction/FullTimeEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep.Abstraction
+{
+    public class FullTimeEmployee : BaseEmployee
+    {
+        public FullTimeEmployee() 
+        { 
+           Console.WriteLine(nameof(FullTimeEmployee));
+        }
+
+        ~FullTimeEmployee()
+        {
+            Console.WriteLine($"Destructor of {nameof(FullTimeEmployee)}");
+        }
+
+        public double AnnualSalary { get; set; }
+
+        public override double MonthlySalary()
+        {
+            FullTimeEmployee ff = new FullTimeEmployee();
+            ff.AnnualSalary = 100000;
+
+            return ff.AnnualSalary/12;
+        }
+    }
+}
